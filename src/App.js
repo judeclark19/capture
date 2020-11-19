88ed61ba5eb31dc35ce2376cfb9c7acea9eec2da
Jude Clark <judeclark2019@gmail.com>
2020-11-19 17:18:08 -0500

    react router

--- src/App.js
+++ src/App.js
@@ -1,18 +1,32 @@
-import "./App.css";
-
 import GlobalStyle from "./components/GlobalStyle";
 
-import Nav from "./components/Nav";
+//Router
+import { Switch, Route } from "react-router-dom";
 
 //Pages
 import AboutUs from "./pages/AboutUs";
+import ContactUs from "./pages/ContactUs";
+import OurWork from "./pages/OurWork";
+
+//Components
+import Nav from "./components/Nav";
 
 function App() {
   return (
     <div className="App">
       <GlobalStyle />
       <Nav />
-      <AboutUs />
+      <Switch>
+        <Route exact path="/">
+          <AboutUs />
+        </Route>
+        <Route path="/work">
+          <OurWork />
+        </Route>
+        <Route path="/contact">
+          <ContactUs />
+        </Route>
+      </Switch>
     </div>
   );
 }
