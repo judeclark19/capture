29bf51167635a0b293d471019746df3f9d8b4150
Jude Clark <judeclark2019@gmail.com>
2020-11-19 16:42:22 -0500

    styled FAQ

--- src/components/FAQ.jsx
+++ src/components/FAQ.jsx
@@ -1,9 +1,14 @@
 import React from "react";
 
+import styled from "styled-components";
+import { SectionDiv } from "../styles";
+
 function FAQ() {
   return (
-    <div className="faq-section">
-      <h2>Frequently Asked Questions</h2>
+    <FaqDiv>
+      <h2>
+        <span>Frequently Asked Questions</span>
+      </h2>
       <div className="question">
         <h4>How do I start?</h4>
         <div className="answer">
@@ -13,6 +18,7 @@ function FAQ() {
             praesentium?
           </p>
         </div>
+        <div className="faq-line"></div>
       </div>
       <div className="question">
         <h4>What is the process?</h4>
@@ -23,6 +29,7 @@ function FAQ() {
             praesentium?
           </p>
         </div>
+        <div className="faq-line"></div>
       </div>
       <div className="question">
         <h4>How much does it cost?</h4>
@@ -33,6 +40,7 @@ function FAQ() {
             praesentium?
           </p>
         </div>
+        <div className="faq-line"></div>
         <div className="question">
           <h4>When will I get my finished film?</h4>
           <div className="answer">
@@ -42,10 +50,39 @@ function FAQ() {
               praesentium?
             </p>
           </div>
+          <div className="faq-line"></div>
         </div>
       </div>
-    </div>
+    </FaqDiv>
   );
 }
+
+const FaqDiv = styled(SectionDiv)`
+  display: block;
+
+  h2 {
+    padding-bottom: 2rem;
+    font-weight: lighter;
+  }
+
+  .faq-line {
+    background: #cccccc;
+    height: 0.2rem;
+    margin: 2rem 0rem;
+    width: 100%;
+  }
+
+  .question {
+    padding: 3rem 0rem;
+    cursor: pointer;
+  }
+
+  .answer {
+    padding: 2rem 0rem;
+    p {
+      padding: 1rem 0rem;
+    }
+  }
+`;
 
 export default FAQ;
--- src/components/GlobalStyle.jsx
+++ src/components/GlobalStyle.jsx
@@ -39,7 +39,7 @@ h2{
 h4{
   font-weight: bold;
   
-
+font-size: 2rem;
 }
 
 span{
